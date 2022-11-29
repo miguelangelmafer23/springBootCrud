08f6cd897175f041e8d38324c8518021f12b8318
Miguel Ángel Martín <mamartinf@minsait.com>
2022-11-29 17:17:47 +0100

    First commit

---
+++ src/main/java/com/indra/curso/EjercicioSpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.indra.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioSpringBootApplication.class, args);
+		
+	}
+	
+	
+
+}
---
+++ src/main/java/com/indra/curso/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.indra.curso;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EjercicioSpringBootApplication.class);
+	}
+
+}
